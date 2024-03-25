a8adf6f1e51153479d3245887f58bbb46bedc356
DeokJae Kim <151099214+kdj0712@users.noreply.github.com>
2024-03-25 05:02:20 +0000

    commit

--- src/quests/ForsIfs.java
+++ src/quests/ForsIfs.java
@@ -6,9 +6,9 @@ public class ForsIfs {
         Scanner Scanner = new Scanner(System.in);
         System.out.print("숫자를 입력하세요 : ");
         int number = Scanner.nextInt();
-        for (int count = 1 ; count <= number ; count++){
+        for (int count = 1 ; count <= number ; count = count +1 ){
             int result =(int)Math.pow(2,count);
-            if (result % 4 == 0){// Good morning : 6 ~ 11
+            if (result % 8 == 0){// Good morning : 6 ~ 11
                 System.out.println(result);
             } else{            
             }
