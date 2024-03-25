e786338be8d2f00e892a735297518a1d5fce0600
DeokJae Kim <151099214+kdj0712@users.noreply.github.com>
2024-03-25 06:35:01 +0000

    commit

--- src/quests/WhilesIfsBreak.java
+++ src/quests/WhilesIfsBreak.java
@@ -1,22 +1,22 @@
 package quests;
 import java.util.Scanner;
 
-public class WhilesIfs {
+public class WhilesIfsBreak {
     public static void main(String[] args) {
         boolean whileFlag = true;
         while (whileFlag) {
             Scanner Scanner = new Scanner(System.in);
             System.out.print("숫자를 입력하세요 : ");
             int number = Scanner.nextInt();
-            if (90<=number && number<=100){// Good morning : 6 ~ 11
+            if (90<=number && number<=100){
                 System.out.println("당신의 학점은 A입니다");
-            } else if (80<=number && number<=89){// Good morning : 6 ~ 11
+            } else if (80<=number && number<=89){
                 System.out.println("당신의 학점은 B입니다");
-            } else if (70<=number && number<=79){// Good morning : 6 ~ 11
+            } else if (70<=number && number<=79){
                 System.out.println("당신의 학점은 C입니다");
-            } else if (0<number && number<=69){// Good morning : 6 ~ 11
+            } else if (0<number && number<=69){
                 System.out.println("당신의 학점은 F입니다");
-            } else if (number == -1){// Good morning : 6 ~ 11
+            } else if (number == -1){
                 break;
             }
         }
